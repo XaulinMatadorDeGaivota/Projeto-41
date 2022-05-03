a9e0cb0c4765c5f0a72680c3298e62d05ee74f33
Matheus Cabral Calil <93673317+XaulinMatadorDeGaivota@users.noreply.github.com>
2022-05-03 11:53:05 -0300

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIÁVEL";
+const accessCode2 = "FUNÇÃO";
+const accessCode3 = "BANCO DE DADOS";
+
+function clues() {
+  fill("white");
+  textSize(15);
+  text("R E V A I L V Á", 100,50)
+  fill("lightblue");
+  text("Dica: Sempre mudando, não constante!", 100, 70);
+
+  // Adicione código para exibir as outras duas perguntas.
+  fill("white");
+  textSize(15);
+  text("N Ç F U Ã O", 700,150)
+  fill("lightblue");
+  text("Dica: Perdorma uma tarefa em particular", 650, 170);
+
+  fill("white");
+  textSize(15);
+  text("N O C B A  E D  O S A D D", 100, 250)
+  fill("lightblue");
+  text("Dica: Armazena as informações", 100, 270);
+}
--- sketch.js
+++ sketch.js
@@ -1,54 +1,38 @@
-var database;
-var back_img;
-var gameState =0;
-var playerCount = 0;
-var allPlayers;
-var score =0;
-var player, form,game;
-var player1,player2;
-var players;
-var fruits;
-var fruitGroup;
-var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
-var player_img;
-var player1score =0;
-var player2score =0;
+var bg1, bg2, form, system, code, security;
+var score = 0;
 
-var obstacleGroup,obstacle,obstacleImage;
+function preload() {
+  bg1 = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg")
 
-function preload(){
-  back_img = loadImage("images/jungle.jpg");
-  player_img = loadImage("images/basket2.png");
-  fruit1_img = loadImage("images/apple2.png");
-  fruit2_img = loadImage("images/banana2.png");
-  fruit3_img = loadImage("images/melon2.png");
-  fruit4_img = loadImage("images/orange2.png");
-  fruit5_img = loadImage("images/pineapple2.png");
-  fruitGroup = new Group();
-
-  obstacleImage = loadImage("images/mushroom.png")
+  // Adicione código para carregar imagem do plano de fundo do tesouro
 }
+
 function setup() {
-  createCanvas(1000, 600);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
-  
+  createCanvas(1000, 500);
+  security = new Security();
+  system = new System();
 }
 
 function draw() {
-  background(back_img);
-  
-   if (playerCount === 2) {
-     game.update(1);
-   }
-   if (gameState === 1) {
-     clear(); 
-     game.play();
-   }
-   if (gameState === 2) {
+  background(bg1);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Pontuação: " + score, 450, 50);
+
+  if (score === 3) {
+    clear();
+    background(bg2);
+    fill("white");
+    textSize(40);
+    text("Tesouro encontrado", 250,200)
+    // Adicione código para alterar o plano de fundo para o plano de fundo do tesouro
+
+    // Adicione código para exibir texto "TESOURO ENCONTRADO"
     
-     game.end();
-   }
+  }
+
+  drawSprites();
 }
